d01ccd855fbfa1f9c6dd71604b9c0684784062be
mbecel <mael1.becel@epitech.eu>
2022-03-22 16:20:25 +0100

    action (workflow): normez -> bubulle

--- src/main.c
+++ src/main.c
@@ -9,7 +9,39 @@
 #include "printf.h"
 #include "rpg.h"
 
-int main (UNUSED int ac,UNUSED char **argv)
+int main (UNUSED int ac, UNUSED char **argv)
 {
+    int a, b;
+    (void)a;
+    (void)b;
+    (void)a;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)b;
+    (void)bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb;
+    (void)b;
+    (void)b;
     return 0;
 }
