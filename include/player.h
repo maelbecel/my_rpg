0145a2f0fdedf131d047e3a79f26858e112424e5
Ehlum-Lucas <lucas.iglesia@epitech.eu>
2022-04-09 17:52:33 +0200

    refactor (include/): add documentation

--- include/player.h
+++ include/player.h
@@ -11,12 +11,29 @@
     typedef struct element_s element_t;
     typedef struct player player_t;
 
+    ////////////////////////////////////////////////////////////
+    /// \brief Player struct.
+    ///
+    /// Contain the element, the hp, the strenght, the speed and the defense
+    /// base stats of the player
+    ///
+    ////////////////////////////////////////////////////////////
     struct player {
         element_t *elem;
-        int pv;
+        int hp;
         int strg;
         int spd;
         int def;
     };
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Initialize the player with it's base stats
+    ///
+    /// \param class Name of the class of the player
+    ///
+    /// \return struct player_t* with the player base stats
+    ///
+    ////////////////////////////////////////////////////////////
+    player_t *init_player(char *class);
 
 #endif /* !PLAYER_H_ */
