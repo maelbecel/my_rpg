d621d6e2def60c3f7b58f116757193b71015071f
mbecel <mael1.becel@epitech.eu>
2022-04-12 16:31:55 +0200

    feat (menu.c): add animation on menu

--- src/display/display_menu_player.c
+++ src/display/display_menu_player.c
@@ -9,16 +9,16 @@
 #include "printf.h"
 #include "rpg.h"
 
-void display_stat(game_t *game, sfEvent *event)
+void display_player(game_t *game, sfEvent *event)
 {
     for (int e = 0; game->scenes[MENU_PLAYER].elements[e]; e++)
         draw_element(game->window, game->scenes[MENU_PLAYER].elements[e]);
     for (int b = 0; game->scenes[MENU_PLAYER].buttons[b]; b++) {
         if (event->type == sfEvtMouseButtonPressed &&
-            is_click(game, b, MENU_PLAYER)) {
+            is_click(game, b, MENU_PLAYER) && b != 0) {
             draw_clicked(game->window, game->scenes[MENU_PLAYER].buttons[b]);
             game->scenes[MENU_PLAYER].buttons[b]->action_clicked(game, b);
-        } else if (is_hoover(game, b, MENU_PLAYER))
+        } else if (is_hoover(game, b, MENU_PLAYER) && b != 0)
             draw_hoover(game->window, game->scenes[MENU_PLAYER].buttons[b]);
         else
             draw_button(game->window, game->scenes[MENU_PLAYER].buttons[b]);
@@ -35,23 +35,66 @@ void display_inventory(game_t *game, sfEvent *event)
         draw_element(game->window, game->scenes[MENU_PLAYER].elements[e]);
     for (int b = 0; game->scenes[MENU_PLAYER].buttons[b]; b++) {
         if (event->type == sfEvtMouseButtonPressed &&
-            is_click(game, b, MENU_PLAYER)) {
+            is_click(game, b, MENU_PLAYER) && b != 3) {
             draw_clicked(game->window, game->scenes[MENU_PLAYER].buttons[b]);
             game->scenes[MENU_PLAYER].buttons[b]->action_clicked(game, b);
-        } else if (is_hoover(game, b, MENU_PLAYER))
+        } else if (is_hoover(game, b, MENU_PLAYER) && b != 3)
             draw_hoover(game->window, game->scenes[MENU_PLAYER].buttons[b]);
         else
             draw_button(game->window, game->scenes[MENU_PLAYER].buttons[b]);
     }
-    return;
+}
+
+void display_stat(game_t *game, sfEvent *event)
+{
+    for (int e = 0; game->scenes[MENU_PLAYER].elements[e]; e++)
+        draw_element(game->window, game->scenes[MENU_PLAYER].elements[e]);
+    for (int b = 0; game->scenes[MENU_PLAYER].buttons[b]; b++) {
+        if (event->type == sfEvtMouseButtonPressed &&
+            is_click(game, b, MENU_PLAYER) && b != 1) {
+            draw_clicked(game->window, game->scenes[MENU_PLAYER].buttons[b]);
+            game->scenes[MENU_PLAYER].buttons[b]->action_clicked(game, b);
+        } else if (is_hoover(game, b, MENU_PLAYER) && b != 1)
+            draw_hoover(game->window, game->scenes[MENU_PLAYER].buttons[b]);
+        else
+            draw_button(game->window, game->scenes[MENU_PLAYER].buttons[b]);
+    }
+}
+
+void display_quest(game_t *game, sfEvent *event)
+{
+    for (int e = 0; game->scenes[MENU_PLAYER].elements[e]; e++)
+        draw_element(game->window, game->scenes[MENU_PLAYER].elements[e]);
+    for (int b = 0; game->scenes[MENU_PLAYER].buttons[b]; b++) {
+        if (event->type == sfEvtMouseButtonPressed &&
+            is_click(game, b, MENU_PLAYER) && b != 2) {
+            draw_clicked(game->window, game->scenes[MENU_PLAYER].buttons[b]);
+            game->scenes[MENU_PLAYER].buttons[b]->action_clicked(game, b);
+        } else if (is_hoover(game, b, MENU_PLAYER) && b != 2)
+            draw_hoover(game->window, game->scenes[MENU_PLAYER].buttons[b]);
+        else
+            draw_button(game->window, game->scenes[MENU_PLAYER].buttons[b]);
+    }
+    draw_text("NO QUEST ACTUALLY", 80, (sfVector2f){400, 600}, game->window);
 }
 
 void display_menu_player(game_t *game, sfEvent *event)
 {
+    for (int i = 0; game->scenes[MENU_PLAYER].buttons[i]; i++){
+        sfTexture_destroy(game->scenes[MENU_PLAYER].buttons[i]->base->texture);
+        game->scenes[MENU_PLAYER].buttons[i]->base->texture = sfTexture_createFromFile("ressources/UI/button1.png", NULL);
+        sfSprite_setTexture(game->scenes[MENU_PLAYER].buttons[i]->base->sprite, game->scenes[MENU_PLAYER].buttons[i]->base->texture, true);
+    }
+    game->scenes[MENU_PLAYER].buttons[game->scenes[MENU_PLAYER].tab->page]->base->texture = sfTexture_createFromFile("ressources/UI/button_menu.png", NULL);
+    sfSprite_setTexture(game->scenes[MENU_PLAYER].buttons[game->scenes[MENU_PLAYER].tab->page]->base->sprite, game->scenes[MENU_PLAYER].buttons[game->scenes[MENU_PLAYER].tab->page]->base->texture, true);
     switch(game->scenes[MENU_PLAYER].tab->page) {
-        case 0: display_stat(game, event);
+        case 0: display_player(game, event);
             break;
-        case 1: display_inventory(game, event);
+        case 1: display_stat(game, event);
+            break;
+        case 2: display_quest(game, event);
+            break;
+        case 3: display_inventory(game, event);
             break;
     }
 }
--- src/initialize/init_menu_player.c
+++ src/initialize/init_menu_player.c
@@ -9,39 +9,61 @@
 #include "printf.h"
 #include "rpg.h"
 
-void go_to_inventory(game_t* game, ...)
-{
-    game->scenes[MENU_PLAYER].tab->page = 1;
-}
-
-void go_to_stat(game_t* game, ...)
+void go_to_player(game_t* game, ...)
 {
     game->scenes[MENU_PLAYER].tab->page = 0;
 }
 
+void go_to_stat(game_t* game, ...)
+{
+    game->scenes[MENU_PLAYER].tab->page = 1;
+}
+
+void go_to_quest(game_t* game, ...)
+{
+    game->scenes[MENU_PLAYER].tab->page = 2;
+}
+
+void go_to_inventory(game_t* game, ...)
+{
+    game->scenes[MENU_PLAYER].tab->page = 3;
+}
+
+
 button_t **menu_player_buttons(void)
 {
-    button_t **buttons = malloc(sizeof(button_t *) * 3);
-    buttons[0] = init_button("STAT", "ressources/UI/button1.png",
-                        (sfVector2f){0, 0}, (sfVector2i){300, 100});
-    buttons[1] = init_button("INVENTORY", "ressources/UI/button1.png",
-                        (sfVector2f){480, 0}, (sfVector2i){300, 100});
-    buttons[2] = NULL;
-    buttons[0]->base->scale = (sfVector2f){0.6, 0.5};
-    buttons[0]->clicked->scale = (sfVector2f){0.6, 0.5};
-    buttons[0]->hoover->scale = (sfVector2f){0.6, 0.5};
-    buttons[0]->action_clicked = go_to_stat;
-    buttons[1]->base->scale = (sfVector2f){0.6, 0.5};
-    buttons[1]->clicked->scale = (sfVector2f){0.6, 0.5};
-    buttons[1]->hoover->scale = (sfVector2f){0.6, 0.5};
-    buttons[1]->action_clicked = go_to_inventory;
+    button_t **buttons = malloc(sizeof(button_t *) * 5);
+    buttons[0] = init_button("PLAYER", "ressources/UI/button1.png",
+                        (sfVector2f){25, 25}, (sfVector2i){300, 100});
+    buttons[1] = init_button("STAT", "ressources/UI/button1.png",
+                        (sfVector2f){492.5, 25}, (sfVector2i){300, 100});
+    buttons[2] = init_button("QUEST", "ressources/UI/button1.png",
+                        (sfVector2f){960, 25}, (sfVector2i){300, 100});
+    buttons[3] = init_button("INVENTORY", "ressources/UI/button1.png",
+                        (sfVector2f){1427.5, 25}, (sfVector2i){300, 100});
+    buttons[4] = NULL;
+    for (int i = 0; i < 4; i++) {
+        buttons[i]->base->scale = (sfVector2f){0.59, 0.5};
+        buttons[i]->clicked->scale = (sfVector2f){0.59, 0.5};
+        buttons[i]->hoover->scale = (sfVector2f){0.59, 0.5};
+        buttons[i]->base->text_pos.x *= 0.59;
+        buttons[i]->base->text_pos.y *= 0.5;
+        buttons[i]->clicked->text_pos.x *= 0.59;
+        buttons[i]->clicked->text_pos.y *= 0.5;
+        buttons[i]->hoover->text_pos.x *= 0.59;
+        buttons[i]->hoover->text_pos.y *= 0.5;
+    }
+    buttons[0]->action_clicked = go_to_player;
+    buttons[1]->action_clicked = go_to_stat;
+    buttons[2]->action_clicked = go_to_quest;
+    buttons[3]->action_clicked = go_to_inventory;
     return buttons;
 }
 
 element_t **menu_player_elements(void)
 {
     element_t **elements = malloc(sizeof(element_t *) * 3);
-    elements[0] = init_element("ressources/menu/bg_menu.png",
+    elements[0] = init_element("ressources/menu/resume.png",
             (sfVector2f){0, 0}, (sfVector2f){1920, 1080}, (sfVector2f){1, 1});
     elements[1] = NULL;
     elements[0]->rect = (sfIntRect){0, 0, 1920, 1080};
