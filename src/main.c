58d17b4899ec750d970a4113cc617c063502abd2
mbecel <mael1.becel@epitech.eu>
2022-04-14 09:49:09 +0200

    feat (main.c): set to framerate dependant

--- src/main.c
+++ src/main.c
@@ -32,7 +32,7 @@ game_t *init_game(void)
     game->settings = init_settings();
     game->window = sfRenderWindow_create(mode, "RPG no seed",
                                                         sfFullscreen, NULL);
-    sfRenderWindow_setFramerateLimit(game->window, 30);
+    sfRenderWindow_setFramerateLimit(game->window, my_getnbr(parser("config/config.json", "framerate")));
     intro(game->window);
     game->scenes = init_scenes(game->window);
     game->player = init_player("chevalier");
