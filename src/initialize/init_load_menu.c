bdd41817d512fce02f7e7515ee8cb0b6ac6681cd
mbecel <mael1.becel@epitech.eu>
2022-04-16 01:16:40 +0200

    feat (load_menu.c): add button to clear a save for #3

--- src/initialize/init_load_menu.c
+++ src/initialize/init_load_menu.c
@@ -9,6 +9,42 @@
 #include "printf.h"
 #include "rpg.h"
 
+void clear_1(UNUSED game_t *game, ...)
+{
+    char *file = my_strdup("config/save1.json");
+    update_file(file, "new", "1");
+    update_file(file, "class", "null");
+    update_file(file, "stength", "0");
+    update_file(file, "speed", "0");
+    update_file(file, "defense", "0");
+    update_file(file, "health", "0");
+    update_file(file, "point_stat", "0");
+}
+
+void clear_2(UNUSED game_t *game, ...)
+{
+    char *file = my_strdup("config/save2.json");
+    update_file(file, "new", "1");
+    update_file(file, "class", "null");
+    update_file(file, "stength", "0");
+    update_file(file, "speed", "0");
+    update_file(file, "defense", "0");
+    update_file(file, "health", "0");
+    update_file(file, "point_stat", "0");
+}
+
+void clear_3(UNUSED game_t *game, ...)
+{
+    char *file = my_strdup("config/save3.json");
+    update_file(file, "new", "1");
+    update_file(file, "class", "null");
+    update_file(file, "stength", "0");
+    update_file(file, "speed", "0");
+    update_file(file, "defense", "0");
+    update_file(file, "health", "0");
+    update_file(file, "point_stat", "0");
+}
+
 void load_1(game_t *game, ...)
 {
     game->player->save[0] = '1';
@@ -58,7 +94,7 @@ element_t **load_menu_elements(void)
 
 button_t **load_menu_buttons(void)
 {
-    button_t **buttons = malloc(sizeof(button_t *) * 4);
+    button_t **buttons = malloc(sizeof(button_t *) * 7);
 
     buttons[0] = init_button("SAVE 1", "ressources/UI/button1.png",
                         (sfVector2f){50, 150}, (sfVector2i){300, 100});
@@ -69,9 +105,26 @@ button_t **load_menu_buttons(void)
     buttons[2] = init_button("SAVE 3", "ressources/UI/button1.png",
                         (sfVector2f){50, 650}, (sfVector2i){300, 100});
     buttons[2]->action_clicked = load_3;
-    buttons[3] = NULL;
+    buttons[3] = init_button("CLEAR", "ressources/UI/button1.png",
+                        (sfVector2f){850, 150}, (sfVector2i){300, 100});
+    buttons[3]->action_clicked = clear_1;
+    buttons[4] = init_button("CLEAR", "ressources/UI/button1.png",
+                        (sfVector2f){850, 400}, (sfVector2i){300, 100});
+    buttons[4]->action_clicked = clear_2;
+    buttons[5] = init_button("CLEAR", "ressources/UI/button1.png",
+                        (sfVector2f){850, 650}, (sfVector2i){300, 100});
+    buttons[5]->action_clicked = clear_3;
+    buttons[6] = NULL;
     buttons[0]->action_clicked = (my_getnbr(parser("saves/save1.json", "new")) == 1) ? new_game : buttons[0]->action_clicked;
     buttons[1]->action_clicked = (my_getnbr(parser("saves/save2.json", "new")) == 1) ? new_game : buttons[1]->action_clicked;
     buttons[2]->action_clicked = (my_getnbr(parser("saves/save3.json", "new")) == 1) ? new_game : buttons[2]->action_clicked;
+    for (int i = 3; buttons[i]; i++) {
+        buttons[i]->base->scale = (sfVector2f){0.3, 1};
+        buttons[i]->base->text_pos = (sfVector2f){buttons[i]->base->text_pos.x - 200, buttons[i]->base->text_pos.y};
+        buttons[i]->hoover->scale = (sfVector2f){0.3, 1};
+        buttons[i]->hoover->text_pos = (sfVector2f){buttons[i]->hoover->text_pos.x - 200, buttons[i]->hoover->text_pos.y};
+        buttons[i]->clicked->scale = (sfVector2f){0.3, 1};
+        buttons[i]->clicked->text_pos = (sfVector2f){buttons[i]->clicked->text_pos.x - 200, buttons[i]->clicked->text_pos.y};
+    }
     return buttons;
 }
