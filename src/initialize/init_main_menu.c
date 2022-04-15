676f5dcf6f00f967e994e5d06d6a718a1b0de98f
mbecel <mael1.becel@epitech.eu>
2022-04-15 16:42:21 +0200

    fix (main_menu): segv when click on two buttons

--- src/initialize/init_main_menu.c
+++ src/initialize/init_main_menu.c
@@ -29,16 +29,16 @@ button_t **main_menu_buttons(void)
     button_t **buttons = malloc(sizeof(button_t *) * 5);
 
     buttons[0] = init_button("PLAY", "ressources/UI/button1.png",
-                        (sfVector2f){1200, 100}, (sfVector2i){300, 100});
+                        (sfVector2f){1200, 90}, (sfVector2i){300, 100});
     buttons[0]->action_clicked = load;
     buttons[1] = init_button("HELP", "ressources/UI/button1.png",
                         (sfVector2f){1200, 300}, (sfVector2i){300, 100});
     buttons[1]->action_clicked = htp_first;
     buttons[2] = init_button("SETTINGS", "ressources/UI/button1.png",
-                        (sfVector2f){1200, 500}, (sfVector2i){300, 100});
+                        (sfVector2f){1200, 510}, (sfVector2i){300, 100});
     buttons[2]->action_clicked = settings;
     buttons[3] = init_button("QUIT", "ressources/UI/button1.png",
-                        (sfVector2f){1200, 700}, (sfVector2i){300, 100});
+                        (sfVector2f){1200, 720}, (sfVector2i){300, 100});
     buttons[3]->action_clicked = quit;
     buttons[4] = NULL;
     return buttons;
