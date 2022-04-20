3874e88b1559e12f53b602d36ff604f90183e760
ZeNo <enzo.garnier@epitech.eu>
2022-04-20 15:06:58 +0200

    feat: add sprites of player and add they in code

--- include/player.h
+++ include/player.h
@@ -8,6 +8,13 @@
 #ifndef PLAYER_H_
     #define PLAYER_H_
 
+    #define CROSSBOW_P "ressources/player/crossbow_man.png"
+    #define ARCHER_P "ressources/player/bow_man.png"
+    #define COOKER_P "ressources/player/cooker.png"
+    #define KNIGHT_P "ressources/player/knight.png"
+    #define VALKYRIE_P "ressources/player/valkyrie.png"
+    #define MAGE_P "ressources/player/mage.png"
+
     typedef struct element_s element_t;
     typedef struct player player_t;
 
