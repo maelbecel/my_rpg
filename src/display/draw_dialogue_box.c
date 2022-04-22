91240eaecc5d7040f6035d4be271975c3cdbe38d
ZeNo <enzo.garnier@epitech.eu>
2022-04-22 22:06:37 +0200

    fix: fix the rect of dialogue box

--- src/display/draw_dialogue_box.c
+++ src/display/draw_dialogue_box.c
@@ -13,8 +13,8 @@ void draw_dialogue_box(sfRenderWindow *window, char *str)
 {
     sfRectangleShape *rect = sfRectangleShape_create();
 
-    sfRectangleShape_setPosition(rect, (sfVector2f) {0, 0});
-    sfRectangleShape_setSize(rect, (sfVector2f) {1920, 930});
+    sfRectangleShape_setPosition(rect, (sfVector2f) {0, 930});
+    sfRectangleShape_setSize(rect, (sfVector2f) {1920, 150});
     sfRectangleShape_setFillColor(rect, sfColor_fromRGBA(255, 255, 255, 100));
     sfRenderWindow_drawRectangleShape(window, rect, NULL);
     draw_text_white(str, 60, (sfVector2f) {15, 930}, window);
