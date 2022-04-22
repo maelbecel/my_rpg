398928f750de039637273bf688c9d60b1deda5c4
Ehlum-Lucas <lucas.iglesia@epitech.eu>
2022-04-23 00:31:48 +0200

    feat (all): add other npc

--- src/initialize/init_npc.c
+++ src/initialize/init_npc.c
@@ -11,10 +11,13 @@
 
 npc_t **game_npc(void)
 {
-    npc_t **npc = malloc(sizeof(npc_t *) * (2 + 1));
+    npc_t **npc = malloc(sizeof(npc_t *) * (5 + 1));
 
     npc[0] = create_npc("bob");
     npc[1] = create_npc("erwann");
-    npc[2] = NULL;
+    npc[2] = create_npc("marine_lepen");
+    npc[3] = create_npc("pascal");
+    npc[4] = create_npc("patrick");
+    npc[5] = NULL;
     return npc;
 }
