e41e37e045c54709638dbd5b6d27892f9310ee9d
Ehlum-Lucas <lucas.iglesia@epitech.eu>
2022-04-25 23:29:06 +0200

    feat (src/): add menu merchant

--- src/initialize/init_talk_npc.c
+++ src/initialize/init_talk_npc.c
@@ -11,9 +11,11 @@
 
 element_t **npc_elements(void)
 {
-    element_t **elements = malloc(sizeof(element_t *) * (1));
+    element_t **elements = malloc(sizeof(element_t *) * (2 + 1));
 
-    elements[1] = NULL;
+    elements[1] = init_element("assets/ui/button1.png", (sfVector2f){600, 0},
+                                (sfVector2f){793, 206}, (sfVector2f){3, 8});
+    elements[2] = NULL;
     return elements;
 }
 
