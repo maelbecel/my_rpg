4e1c86ecafb59abc2c73520679ddf89cd8c8c4ff
mbecel <mael1.becel@epitech.eu>
2022-04-26 13:56:29 +0200

    feat (npc/): move player when talking to an npc ✨

--- src/display/display_npc.c
+++ src/display/display_npc.c
@@ -21,9 +21,11 @@ npc_t *find_npc(game_t *game)
 {
     int dist = 0;
     sfVector2f playerpos = {game->scenes[GAME].elements[0]->rect.left +
-                                    game->scenes[GAME].elements[2]->pos.x,
+                            game->scenes[GAME].elements[2]->rect.width / 2 +
+                            game->scenes[GAME].elements[2]->pos.x,
                             game->scenes[GAME].elements[0]->rect.top +
-                                    game->scenes[GAME].elements[2]->pos.y};
+                            game->scenes[GAME].elements[2]->rect.height / 2 +
+                            game->scenes[GAME].elements[2]->pos.y};
     npc_t **npc = game->scenes[GAME].npc;
     sfVector2f npcpos;
 
